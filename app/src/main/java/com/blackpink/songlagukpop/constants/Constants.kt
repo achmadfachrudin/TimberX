d1b645bb08f440cff829f979ca96c338b18b6f14
Achmad Fachrudin <a2fachrudin@gmail.com>
2020-12-26 07:06:09 +0700

    update

--- app/src/main/java/com/blackpink/songlagukpop/constants/Constants.kt
+++ app/src/main/java/com/blackpink/songlagukpop/constants/Constants.kt
@@ -41,6 +41,6 @@ object Constants {
     const val ACTION_PLAY_PAUSE = "action_play_pause"
     const val ACTION_NEXT = "action_next"
     const val ACTION_PREVIOUS = "action_previous"
-    const val APP_PACKAGE_NAME = "com.happyproject.blackpinkplay"
+    const val APP_PACKAGE_NAME = "com.blackpink.songlagukpop"
     const val ARTIST_NAME = "BLACKPINK"
 }
